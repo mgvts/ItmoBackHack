446aa68b064ee27d1139a83dd851e9818a37e018
mgvts <89970331+mgvts@users.noreply.github.com>
2023-04-16 12:01:53 +0300

    add basic logic

--- app.py
+++ app.py
@@ -5,29 +5,27 @@ app = Flask(__name__)
 
 base = bd.BDController()
 
-
-def parseData(d: [[]]) -> {"": [{}]}:
-    return {i[3]: i for i in d}
+facts = ["start_time", "data_size", "data_per_time", "duration"]
 
 
 @app.route('/start_time', methods=["GET"])
 def get_data1():
-    return parseData(base.getAll()["start_time"])
+    return base.getAll()["start_time"]
 
 
 @app.route('/data_size', methods=["GET"])
 def get_data2():
-    return parseData(base.getAll()["data_size"])
+    return base.getAll()["data_size"]
 
 
 @app.route('/data_per_time', methods=["GET"])
 def get_data3():
-    return parseData(base.getAll()["data_per_time"])
+    return base.getAll()["data_per_time"]
 
 
 @app.route('/duration', methods=["GET"])
 def get_data4():
-    return parseData(base.getAll()["duration"])
+    return base.getAll()
 
 
 # todo
@@ -41,7 +39,8 @@ def post_data():
 
 
 if __name__ == '__main__':
+    # app.run()
     app.run(host="164.92.210.100")
-# print(parseData([[1, 1111111, 99, "Hi", "hi/hello", 123], [2, 123123, 8989, "hihi", "/hi/men/hi", None],
-#                  [3, 97989, 892828, "ioio", "/ioi/www/www/oo", 9]]))
-    app.run()
+# print(parseData([{"id": "3642626793764804437", "start_time": "1681634569956", "duration": "637", "name": "Test",
+#                   "library": "OkHttp3", "type": "Get", "url": "https://cat-fact.herokuapp.com/facts",
+#                   "data_size": "null", "exception": "null", "stack_trace": ""}]))
